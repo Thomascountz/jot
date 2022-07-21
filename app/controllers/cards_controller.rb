1834e4d1c75f085be9d451cb047b7cc3b3011a69
Thomas Countz <thomascountz@gmail.com>
2022-07-21 17:14:18 +0200

    crud cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,4 +6,39 @@ class CardsController < ApplicationController
   def new
     @card = Card.new
   end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    if @card.update(card_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:body, :bookmark)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "cards#index"
-  get "/author", to: "cards#new"
+  root 'cards#index'
+  resources :cards, except: :show, path: :author
 end
