1c51355457e7fc0dad0da4a623b062f2ec1419ed
Thomas Countz <thomascountz@gmail.com>
2022-07-21 16:23:14 +0200

    globally set max-width of images

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,19 +1,9 @@
 require 'redcarpet'
-require 'action_view'
 
 module CardsHelper
-  class CustomRender < Redcarpet::Render::HTML
-    include ActionView::Helpers::AssetTagHelper
-
-    # Ensure rendered images' widths are capped
-    def image(link, title, alt_text)
-      image_tag(link, title: title, alt: alt_text, style: "max-width:100%")
-    end
-  end
-
   def card_body_html(card)
     markdown = Redcarpet::Markdown.new(
-      CustomRender,
+      Redcarpet::Render::HTML,
       autolink: true,
       tables: true,
       fenced_code_blocks: true,
--- app/javascript/controllers/markdown_editor_controller.js
+++ app/javascript/controllers/markdown_editor_controller.js
@@ -1,17 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 import { default as EasyMDE } from "easymde";
 import hljs from "highlight.js/lib/common";
-import marked from 'marked'
 
 export default class extends Controller {
   connect() {
-
-    // Ensure rendered images' widths are capped in preview
-    const renderer = new marked.Renderer();
-    renderer.image = function (href, title, text) {
-      return `<img src="${href}" alt="${text}" title="${title}" style="max-width:100%;" />`;
-    }
-
     new EasyMDE({
       element: this.element,
       minHeight: "100px",
@@ -20,9 +12,6 @@ export default class extends Controller {
       renderingConfig: {
         codeSyntaxHighlighting: true,
         hljs: hljs, // Use the same highlight.js as renderer
-        markedOptions: {
-          renderer: renderer
-        }
       },
     });
 
